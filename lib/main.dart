639a1da82ba1c94e2a4a315b8882905c98536aa6
karita <karita@souhoudou.jp>
2022-01-31 13:24:57 +0900

    todo追加画面作成

--- lib/main.dart
+++ lib/main.dart
@@ -66,13 +66,40 @@ class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        child: Text('リスト追加画面(クリックで戻る)'),
-      ),
-    ));
+        appBar: AppBar(
+          title: Text('リスト追加'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(64),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(),
+              const SizedBox(height: 8),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  // color: Colors.blue,
+                  onPressed: () {},
+                  child: Text(
+                    'リスト追加',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Container(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('キャンセル'),
+                  ))
+            ],
+          ),
+        ));
   }
 }
